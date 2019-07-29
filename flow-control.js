73f8422582707788004dd8ea5c6bafabc4f7f1c4
Kaitlin Patterson <kaitlin.patterson@observepoint.com>
2019-07-29 17:37:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,5 @@
-function basicTeenager(age) {if (age===[13-19]){say("You are a teenager!")}
-
-
+function basicTeenager(age) {
+if (age === [13-19]) {return "You are a teenager!"}
 }
 
 function teenager(age) {
