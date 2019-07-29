b864094f1ecef06a4767232db2ce6c472a8ce5c0
Kaitlin Patterson <kaitlin.patterson@observepoint.com>
2019-07-29 17:50:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,7 +7,7 @@ if (age >=13 && age<=19) {return "You are a teenager!"} else {return "You are no
 }
 
 function ageChecker(age) {
-
+if (age <13) {return "You are a kid"} else if (age >=13 && age<=19) {return "You are a teenager!"} else {return "You are a grownup"}
 }
 
 function ternaryTeenager(age) {
